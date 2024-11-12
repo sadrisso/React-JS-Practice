8fc8a710b6825d1a43e826e1265d44c8685f494e
sadrisso <sadriisso304@gmail.com>
2024-11-12 12:58:56 +0600

    add an observer to watch users activities

--- auth-practice/src/components/AuthProvider.jsx
+++ auth-practice/src/components/AuthProvider.jsx
@@ -1,10 +1,12 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase.init";
 
 export const AuthContext = createContext(1)
 
 const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
 
     const signUpUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
@@ -18,10 +20,28 @@ const AuthProvider = ({children}) => {
         return signOut(auth);
     }
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                console.log("User exists")
+                setUser(currentUser)
+            }
+            else {
+                console.log("User not exists")
+                setUser(null)
+            }
+        })
+
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
     const contextInfo = {
         signUpUser,
         signInUser,
         signOutUser,
+        user,
     }
 
     return (
